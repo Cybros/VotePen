4edea485b8fd431d752eec67c37f771124ad0961
Yoginth <yoginth@zoho.com>
2018-03-29 06:13:11 +0000

    Rollbacked Double \n

--- resources/views/vendor/notifications/email-plain.blade.php
+++ resources/views/vendor/notifications/email-plain.blade.php
@@ -1,21 +1,21 @@
 <?php
 
 if (! empty($greeting)) {
-    echo $greeting, "\n";
+    echo $greeting, "\n\n";
 } else {
-    echo $level == 'error' ? 'Whoops!' : 'Hello!', "\n";
+    echo $level == 'error' ? 'Whoops!' : 'Hello!', "\n\n";
 }
 
 if (! empty($introLines)) {
-    echo implode("\n", $introLines), "\n";
+    echo implode("\n", $introLines), "\n\n";
 }
 
 if (isset($actionText)) {
-    echo "{$actionText}: {$actionUrl}", "\n";
+    echo "{$actionText}: {$actionUrl}", "\n\n";
 }
 
 if (! empty($outroLines)) {
-    echo implode("\n", $outroLines), "\n";
+    echo implode("\n", $outroLines), "\n\n";
 }
 
 echo 'Regards,', "\n";
